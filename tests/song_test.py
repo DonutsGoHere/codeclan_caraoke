5e4fb83ba0c3475e1192c4c64104eba8ae20e4b0
Calum Cook <calumcook@Calums-Air-2.broadband>
2022-05-29 11:49:38 +0100

    2nd - Songs passing

---
+++ tests/song_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+from src.song import Song
+
+class TestSong(unittest.TestCase):
+    def setUp(self):
+        self.song = Song("You\'re the best around", "Joe Esposito")
+
+    def test_has_song_got_name(self):
+        self.assertEqual("You\'re the best around", self.song.name)
+
+    def test_song_has_artist(self):
+        self.assertEqual("Joe Esposito", self.song.artist)
